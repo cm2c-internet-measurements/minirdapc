c9d5926d60606e6762db08b8fbc3b5790323da00
Carlos M. Martinez <carlos@lacnic.net>
2019-07-20 12:03:13 -0400

    commit

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 # Setup for minirdapc
 # 20190720 v1
 
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 
 setup(name='minirdapc',
-      version='0.2.0',
+      version='0.2.1',
       description='cm2c Mini RDAP Client',
       url='https://github.com/cm2c-internet-measurements/minirdapc',
       author='Carlos M. Martinez',
       author_email='carlos@lacnic.net',
       license='BSD',
-      packages=['minirdapc'],
+      packages=find_namespace_packages(where='.'),
       install_requires=['ipaddr', 'pyjq'],
       zip_safe=False)
